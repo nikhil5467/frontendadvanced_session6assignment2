36e1d6f788a4dd7c067d3061615ac34b17e43b39
nikhil5467 <nikhilrocks374@gmail.com>
2017-05-22 20:44:52 +0530

    session6assignment2
    session6assignment2

---
+++ seed2/seed2/newcomp/newcomp.component.ts
@@ -0,0 +1,11 @@
+import {Component,Input, Output, EventEmitter} from '@angular/core';
+
+const template: string = require('./newcomp.html');
+
+@Component({
+    selector: 'newcomp',
+    template,
+    directives: []
+    })
+
+export class newcomp {}
